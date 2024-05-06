15a09265289ef1c3501ad0776b5a183b00c3704d
Alejandro Guerrero <Gu_Alejandro.007@csu.fullerton.edu>
2024-05-05 21:12:28 -0700

    Worked on the contact page modules. Created a basic for setup and added some styling to match the rest of the site.

--- portfolio/src/components/Contact/contact.jsx
+++ portfolio/src/components/Contact/contact.jsx
@@ -3,7 +3,19 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <div>contact</div>
+    <section id='contact'>
+      <div className='contact'>
+        <h1 className='contactTitle'>Contact Me</h1>
+        <span className='contactDesc'> Please fill out the form to contact me</span>
+        <form className='contactForm'>
+          <input type='text' className='name' placeholder='Enter your name' />
+          <input type='email' className='email' placeholder='Enter your email' />
+          <input type='tel' className='number' placeholder='Enter your telephone number'/>
+          <textarea className='message' name='message' rows='4' placeholder='Enter a message' />
+          <button type='submit' value='Send' className='submitButton'>Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
